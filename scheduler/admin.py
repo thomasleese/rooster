e587d591688e8d729f4522b9397995d6cf960485
Thomas Leese <inbox@thomasleese.me>
2016-01-23 11:03:55 +0000

    Add inital model structure

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ConstraintTemplate, Constraint, Event, Volunteer, Job
+
+
+@admin.register(ConstraintTemplate)
+class ConstraintTemplateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Volunteer)
+class VolunteerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    pass
