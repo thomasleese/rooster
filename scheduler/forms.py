3fb7ead687f07f29f360242eafce03ec371844bd
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2016-01-27 19:53:17 +0000

    change max to target as per spec

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -21,16 +21,16 @@ class VolunteerForm(forms.ModelForm):
                 else:
                     attrs = {
                         'min': resource.min_value,
-                        'max': resource.max_value
+                        'target': resource.target_value
                     }
 
-                    if resource.max_value - resource.min_value <= 20:
+                    if resource.target_value - resource.min_value <= 20:
                         attrs['type'] = 'range'
 
                     widget = forms.NumberInput(attrs=attrs)
 
                     field = forms.IntegerField(min_value=resource.min_value,
-                                               max_value=resource.max_value,
+                                               target_value=resource.target_value,
                                                widget=widget,
                                                initial=str(resource.default_value))
 
