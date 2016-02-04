672c84e0efba31788a38387120adecea16061503
Thomas Leese <inbox@thomasleese.me>
2016-02-04 08:41:38 +0000

    Use template inheritance to avoid duplication
    All the templates should inherit from the base template meaning that
    they all follow a similar look and feel.

--- rooster/settings.py
+++ rooster/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'rooster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'rooster', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
