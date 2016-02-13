7654a81760d228227c3e3ef9ff9cac9927b4674a
Thomas Leese <inbox@thomasleese.me>
2016-02-13 10:49:28 +0000

    Add a test for public names

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Event, Volunteer
+
+
+class VolunteerTestCase(TestCase):
+
+    def test_gets_public_name(self):
+        event = Event.objects.create(name='event', slug='event',
+                                     description='event', slots_per_day=1,
+                                     number_of_days=1)
+        volunteer = Volunteer.objects.create(event=event,
+                                             real_name='Real Name',
+                                             email_address='a@b.c',
+                                             phone_number='123456789')
+        volunteer.ensure_has_public_name()
+        self.assertIsNot(volunteer.public_name, None)
+        self.assertIsNot(volunteer.slug, None)
